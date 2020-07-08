0fecc427a9dae817883c7d4fc8ae0f48058eee57
Kajal Jaiswal <kajalj990@gmail.com>
2020-07-08 21:34:40 +0530

    second commit

--- SampleProj/src/Test.java
+++ SampleProj/src/Test.java
@@ -2,5 +2,6 @@
 public class Test {
   public static void main(String[] args) {
 	System.out.println("Remote Repository using Eclipse GIT plugin ");
+	System.out.println("Remote Repository we are using here is Github");
 }
 }
