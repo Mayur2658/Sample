d4419447ee9633bfb0203d59d6639882888bde5a
Mayur Borkar <mayurborkar20@gmail.com>
2020-07-08 22:21:19 +0530

    third commit

--- SampleProj/src/Test.java
+++ SampleProj/src/Test.java
@@ -1,6 +1,6 @@
 
 public class Test {
   public static void main(String[] args) {
-	System.out.println("Remote Repository using Eclipse GIT plugin ");
+	System.out.println("Remote Repository using Eclipse GIT plugins assignment ");
 }
 }
